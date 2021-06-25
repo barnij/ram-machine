b5a091a9ccdffc965cc2fb235fe3379b21e90198
Skonopi <FilipKnefel@gmail.com>
2021-06-25 17:25:34 +0200

    Moved package.json and package-lock.json to project's root directory. Removed parser implementation. Added Abstraction Syntax Tree in ast.js file.

---
+++ engine-alpha/ast.js
@@ -0,0 +1,58 @@
+function Const(value) {
+    this.value = value;
+}
+function Address(value) {
+    this.value = value;
+}
+function Reference(value) {
+    this.value = value;
+}
+function Label(value) {
+    this.value = value;
+}
+
+function Load(argument) {
+    this.argument = argument;
+}
+function Store(argument) {
+    this.argument = argument;
+}
+function Add(argument) {
+    this.argument = argument;
+}
+function Sub(argument) {
+    this.argument = argument;
+}
+function Mult(argument) {
+    this.argument = argument;
+}
+function Div(argument) {
+    this.argument = argument;
+}
+function Read(argument) {
+    this.argument = argument;
+}
+function Write(argument) {
+    this.argument = argument;
+}
+function Jump(argument) {
+    this.argument = argument;
+}
+function Jgtz(argument) {
+    this.argument = argument;
+}
+function Jzero(argument) {
+    this.argument = argument;
+}
+function Halt() {}
+function Skip() {}
+
+function Combine(instruction, nextInstruction) {
+    this.instruction = instruction;
+    this.nextInstruction = nextInstruction;
+}
+function Program(labels, programTree) {
+    this.labels = labels;
+    this.programTree = programTree;
+}
+
