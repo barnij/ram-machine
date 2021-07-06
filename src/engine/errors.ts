588482a7839f0b23bb03f85ef9041f8e89b94493
Skonopi <FilipKnefel@gmail.com>
2021-07-06 22:47:53 +0200

    Added parser Errors.

--- src/engine/errors.ts
+++ src/engine/errors.ts
@@ -1,2 +1,5 @@
 class InterpreterError extends Error {}
+class ParserError extends Error {}
 export class InputError extends InterpreterError {}
+export class ParserSyntaxError extends ParserError {}
+export class ParserTypeError extends ParserError {}
