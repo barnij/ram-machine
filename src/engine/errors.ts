469bcd1165565b4923495e0aebb03b009c8d932a
Andrzej Tkaczyk <309181@uwr.edu.pl>
2021-07-07 14:41:53 +0200

    added register and runtime errors

--- src/engine/errors.ts
+++ src/engine/errors.ts
@@ -1,2 +1,4 @@
 class InterpreterError extends Error {}
 export class InputError extends InterpreterError {}
+export class RegisterError extends InterpreterError {}
+export class RuntimeError extends InterpreterError {}
