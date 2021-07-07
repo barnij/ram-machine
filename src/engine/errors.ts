466f53de7e58a8ca97c5f095b69c8967c471ff99
Andrzej Tkaczyk <309181@uwr.edu.pl>
2021-07-07 15:17:33 +0200

    added label error

--- src/engine/errors.ts
+++ src/engine/errors.ts
@@ -2,3 +2,4 @@ class InterpreterError extends Error {}
 export class InputError extends InterpreterError {}
 export class RegisterError extends InterpreterError {}
 export class RuntimeError extends InterpreterError {}
+export class LabelError extends InterpreterError {}
