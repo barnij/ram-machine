a3cd07bb5b643fee3f4999384e2fae467c90444f
Andrzej Tkaczyk <309181@uwr.edu.pl>
2021-07-14 12:13:39 +0200

    rewrited interp method definition to interpreter file

--- src/engine/ast.ts
+++ src/engine/ast.ts
@@ -1,7 +1,5 @@
-import {RegisterError, RuntimeError, LabelError} from './errors';
-import {State, Environment} from './environment';
+import {State} from './environment';
 import {Ok} from './status';
-export const ACCUMULATOR = BigInt(0);
 
 export type Argument = Operandum | Label;
 
@@ -15,6 +13,10 @@ export class Reference extends Operandum {}
 
 export class Label {
   constructor(public value: string) {}
+}
+
+interface interpInterface {
+  interp: (state: State) => Ok;
 }
 
 export type Instruction =
@@ -33,372 +35,77 @@ export type Instruction =
   | Skip
   | Combine;
 
-export class Load {
+export class Load implements interpInterface {
   constructor(public argument: Operandum) {}
 
-  interp(state: State): Ok {
-    const arg: Operandum = this.argument;
-    switch (arg.constructor) {
-      case Const:
-        state.environment.registers.set(ACCUMULATOR, arg.value);
-        return new Ok();
-      case Address: {
-        const value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        state.environment.registers.set(ACCUMULATOR, value);
-        return new Ok();
-      }
-      case Reference: {
-        let value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        value = state.environment.registers.get(value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + value);
-        }
-        state.environment.registers.set(ACCUMULATOR, value);
-        return new Ok();
-      }
-      default:
-        return new RuntimeError('invalid argumetn in instruction load');
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Store {
   constructor(public argument: Address | Reference) {}
 
-  interp(state: State): Ok {
-    const arg: Address | Reference = this.argument;
-    switch (arg.constructor) {
-      case Address: {
-        const value = state.environment.registers.get(ACCUMULATOR);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + ACCUMULATOR);
-        }
-        state.environment.registers.set(arg.value, value);
-        return new Ok();
-      }
-      case Reference: {
-        const accValue = state.environment.registers.get(ACCUMULATOR);
-        if (accValue == null) {
-          throw new RegisterError('empty register nr ' + ACCUMULATOR);
-        }
-        let value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        value = state.environment.registers.get(value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + value);
-        }
-        state.environment.registers.set(value, accValue);
-        return new Ok();
-      }
-      default:
-        return new RuntimeError('invalid argumetn in instruction load');
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Add {
   constructor(public argument: Operandum) {}
 
-  interp(state: State): Ok {
-    const accValue = state.environment.registers.get(ACCUMULATOR);
-    if (accValue == null) {
-      throw new RuntimeError('empty register nr ' + ACCUMULATOR);
-    }
-    const arg: Operandum = this.argument;
-    switch (arg.constructor) {
-      case Const:
-        state.environment.registers.set(ACCUMULATOR, accValue + arg.value);
-        return new Ok();
-      case Address: {
-        const value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        state.environment.registers.set(ACCUMULATOR, accValue + value);
-        return new Ok();
-      }
-      case Reference: {
-        let value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        value = state.environment.registers.get(value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + value);
-        }
-        state.environment.registers.set(ACCUMULATOR, accValue + value);
-        return new Ok();
-      }
-      default:
-        return new RuntimeError('invalid argumetn in instruction load');
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Sub {
   constructor(public argument: Operandum) {}
 
-  interp(state: State): Ok {
-    const accValue = state.environment.registers.get(ACCUMULATOR);
-    if (accValue == null) {
-      throw new RuntimeError('empty register nr ' + ACCUMULATOR);
-    }
-    const arg: Operandum = this.argument;
-    switch (arg.constructor) {
-      case Const:
-        state.environment.registers.set(ACCUMULATOR, accValue - arg.value);
-        return new Ok();
-      case Address: {
-        const value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        state.environment.registers.set(ACCUMULATOR, accValue - value);
-        return new Ok();
-      }
-      case Reference: {
-        let value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        value = state.environment.registers.get(value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + value);
-        }
-        state.environment.registers.set(ACCUMULATOR, accValue - value);
-        return new Ok();
-      }
-      default:
-        return new RuntimeError('invalid argumetn in instruction load');
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Mult {
   constructor(public argument: Operandum) {}
 
-  interp(state: State): Ok {
-    const accValue = state.environment.registers.get(ACCUMULATOR);
-    if (accValue == null) {
-      throw new RuntimeError('empty register nr ' + ACCUMULATOR);
-    }
-    const arg: Operandum = this.argument;
-    switch (arg.constructor) {
-      case Const:
-        state.environment.registers.set(ACCUMULATOR, accValue * arg.value);
-        return new Ok();
-      case Address: {
-        const value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        state.environment.registers.set(ACCUMULATOR, accValue * value);
-        return new Ok();
-      }
-      case Reference: {
-        let value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        value = state.environment.registers.get(value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + value);
-        }
-        state.environment.registers.set(ACCUMULATOR, accValue * value);
-        return new Ok();
-      }
-      default:
-        return new RuntimeError('invalid argumetn in instruction load');
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Div {
   constructor(public argument: Operandum) {}
 
-  interp(state: State): Ok {
-    const accValue = state.environment.registers.get(ACCUMULATOR);
-    if (accValue == null) {
-      throw new RuntimeError('empty register nr ' + ACCUMULATOR);
-    }
-    const arg: Operandum = this.argument;
-    switch (arg.constructor) {
-      case Const:
-        if (arg.value === BigInt(0)) {
-          throw new RuntimeError('division by 0');
-        }
-        state.environment.registers.set(ACCUMULATOR, accValue / arg.value);
-        return new Ok();
-      case Address: {
-        const value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        if (value === BigInt(0)) {
-          throw new RuntimeError('division by 0');
-        }
-        state.environment.registers.set(ACCUMULATOR, accValue / value);
-        return new Ok();
-      }
-      case Reference: {
-        let value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        value = state.environment.registers.get(value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + value);
-        }
-        if (value === BigInt(0)) {
-          throw new RuntimeError('division by 0');
-        }
-        state.environment.registers.set(ACCUMULATOR, accValue / value);
-        return new Ok();
-      }
-      default:
-        return new RuntimeError('invalid argumetn in instruction load');
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Read {
   constructor(public argument: Address | Reference) {}
 
-  interp(state: State): Ok {
-    const arg: Address | Reference = this.argument;
-    switch (arg.constructor) {
-      case Address: {
-        const value = state.environment.input.read();
-        state.environment.registers.set(arg.value, value);
-        return new Ok();
-      }
-      case Reference: {
-        const readValue = state.environment.input.read();
-        let value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        value = state.environment.registers.get(value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + value);
-        }
-        state.environment.registers.set(value, readValue);
-        return new Ok();
-      }
-      default:
-        return new RuntimeError('invalid argumetn in instruction load');
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Write {
   constructor(public argument: Operandum) {}
 
-  interp(state: State): Ok {
-    const arg: Operandum = this.argument;
-    switch (arg.constructor) {
-      case Const:
-        state.environment.output.write(arg.value);
-        return new Ok();
-      case Address: {
-        const value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        state.environment.output.write(value);
-        return new Ok();
-      }
-      case Reference: {
-        let value = state.environment.registers.get(arg.value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + arg.value);
-        }
-        value = state.environment.registers.get(value);
-        if (value == null) {
-          throw new RegisterError('empty register nr ' + value);
-        }
-        state.environment.output.write(value);
-        return new Ok();
-      }
-      default:
-        return new RuntimeError('invalid argumetn in instruction load');
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Jump {
   constructor(public argument: Label) {}
 
-  interp(state: State): Ok {
-    const arg: Label = this.argument;
-    const jumpTarget = state.program.labels.get(arg.value);
-    if (jumpTarget == null) {
-      throw new LabelError('unrecognized label ' + arg.value);
-    } else {
-      state.nextInstruction = jumpTarget;
-      return new Ok();
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Jgtz {
   constructor(public argument: Label) {}
 
-  interp(state: State): Ok {
-    const arg: Label = this.argument;
-    const jumpTarget = state.program.labels.get(arg.value);
-    if (jumpTarget == null) {
-      throw new LabelError('unrecognized label ' + arg.value);
-    } else {
-      const value = state.environment.registers.get(ACCUMULATOR);
-      if (value == null) {
-        throw new RegisterError('empty register nr ' + arg.value);
-      } else if (value > BigInt(0)) {
-        state.nextInstruction = jumpTarget;
-      }
-      return new Ok();
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Jzero {
   constructor(public argument: Label) {}
 
-  interp(state: State): Ok {
-    const arg: Label = this.argument;
-    const jumpTarget = state.program.labels.get(arg.value);
-    if (jumpTarget == null) {
-      throw new LabelError('unrecognized label ' + arg.value);
-    } else {
-      const value = state.environment.registers.get(ACCUMULATOR);
-      if (value == null) {
-        throw new RegisterError('empty register nr ' + arg.value);
-      } else if (value === BigInt(0)) {
-        state.nextInstruction = jumpTarget;
-      }
-      return new Ok();
-    }
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Halt {
-  interp(state: State): Ok {
-    state.completed = true;
-    return new Ok();
-  }
+  interp!: (state: State) => Ok;
 }
 export class Skip {
-  interp(state: State) {
-    return new Ok();
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Combine {
@@ -407,10 +114,7 @@ export class Combine {
     public nextInstruction: Instruction
   ) {}
 
-  interp(state: State): Ok {
-    state.nextInstruction = this.nextInstruction;
-    return this.instruction.interp(state);
-  }
+  interp!: (state: State) => Ok;
 }
 
 export class Program {
--- src/engine/interpreter.ts
+++ src/engine/interpreter.ts
@@ -2,53 +2,337 @@ import * as ast from './ast';
 import * as inst from './instructions';
 import {Ok} from './status';
 import {State} from './environment';
-import {RuntimeError} from './errors';
+import {RuntimeError, RegisterError, LabelError} from './errors';
+
+const ACCUMULATOR = BigInt(0);
+
+ast.Load.prototype.interp = function (state) {
+  const arg: ast.Operandum = this.argument;
+  switch (arg.constructor) {
+    case ast.Const:
+      state.environment.registers.set(ACCUMULATOR, arg.value);
+      return new Ok();
+    case ast.Address: {
+      const value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      state.environment.registers.set(ACCUMULATOR, value);
+      return new Ok();
+    }
+    case ast.Reference: {
+      let value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      value = state.environment.registers.get(value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + value);
+      }
+      state.environment.registers.set(ACCUMULATOR, value);
+      return new Ok();
+    }
+    default:
+      return new RuntimeError('invalid argumetn in instruction load');
+  }
+};
+
+ast.Store.prototype.interp = function (state) {
+  const arg: ast.Address | ast.Reference = this.argument;
+  switch (arg.constructor) {
+    case ast.Address: {
+      const value = state.environment.registers.get(ACCUMULATOR);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + ACCUMULATOR);
+      }
+      state.environment.registers.set(arg.value, value);
+      return new Ok();
+    }
+    case ast.Reference: {
+      const accValue = state.environment.registers.get(ACCUMULATOR);
+      if (accValue == null) {
+        throw new RegisterError('empty register nr ' + ACCUMULATOR);
+      }
+      let value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      value = state.environment.registers.get(value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + value);
+      }
+      state.environment.registers.set(value, accValue);
+      return new Ok();
+    }
+    default:
+      return new RuntimeError('invalid argumetn in instruction load');
+  }
+};
+
+ast.Add.prototype.interp = function (state) {
+  const accValue = state.environment.registers.get(ACCUMULATOR);
+  if (accValue == null) {
+    throw new RuntimeError('empty register nr ' + ACCUMULATOR);
+  }
+  const arg: ast.Operandum = this.argument;
+  switch (arg.constructor) {
+    case ast.Const:
+      state.environment.registers.set(ACCUMULATOR, accValue + arg.value);
+      return new Ok();
+    case ast.Address: {
+      const value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      state.environment.registers.set(ACCUMULATOR, accValue + value);
+      return new Ok();
+    }
+    case ast.Reference: {
+      let value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      value = state.environment.registers.get(value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + value);
+      }
+      state.environment.registers.set(ACCUMULATOR, accValue + value);
+      return new Ok();
+    }
+    default:
+      return new RuntimeError('invalid argumetn in instruction load');
+  }
+};
+
+ast.Sub.prototype.interp = function (state) {
+  const accValue = state.environment.registers.get(ACCUMULATOR);
+  if (accValue == null) {
+    throw new RuntimeError('empty register nr ' + ACCUMULATOR);
+  }
+  const arg: ast.Operandum = this.argument;
+  switch (arg.constructor) {
+    case ast.Const:
+      state.environment.registers.set(ACCUMULATOR, accValue - arg.value);
+      return new Ok();
+    case ast.Address: {
+      const value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      state.environment.registers.set(ACCUMULATOR, accValue - value);
+      return new Ok();
+    }
+    case ast.Reference: {
+      let value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      value = state.environment.registers.get(value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + value);
+      }
+      state.environment.registers.set(ACCUMULATOR, accValue - value);
+      return new Ok();
+    }
+    default:
+      return new RuntimeError('invalid argumetn in instruction load');
+  }
+};
+
+ast.Mult.prototype.interp = function (state) {
+  const accValue = state.environment.registers.get(ACCUMULATOR);
+  if (accValue == null) {
+    throw new RuntimeError('empty register nr ' + ACCUMULATOR);
+  }
+  const arg: ast.Operandum = this.argument;
+  switch (arg.constructor) {
+    case ast.Const:
+      state.environment.registers.set(ACCUMULATOR, accValue * arg.value);
+      return new Ok();
+    case ast.Address: {
+      const value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      state.environment.registers.set(ACCUMULATOR, accValue * value);
+      return new Ok();
+    }
+    case ast.Reference: {
+      let value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      value = state.environment.registers.get(value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + value);
+      }
+      state.environment.registers.set(ACCUMULATOR, accValue * value);
+      return new Ok();
+    }
+    default:
+      return new RuntimeError('invalid argumetn in instruction load');
+  }
+};
+
+ast.Div.prototype.interp = function (state) {
+  const accValue = state.environment.registers.get(ACCUMULATOR);
+  if (accValue == null) {
+    throw new RuntimeError('empty register nr ' + ACCUMULATOR);
+  }
+  const arg: ast.Operandum = this.argument;
+  switch (arg.constructor) {
+    case ast.Const:
+      if (arg.value === BigInt(0)) {
+        throw new RuntimeError('division by 0');
+      }
+      state.environment.registers.set(ACCUMULATOR, accValue / arg.value);
+      return new Ok();
+    case ast.Address: {
+      const value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      if (value === BigInt(0)) {
+        throw new RuntimeError('division by 0');
+      }
+      state.environment.registers.set(ACCUMULATOR, accValue / value);
+      return new Ok();
+    }
+    case ast.Reference: {
+      let value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      value = state.environment.registers.get(value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + value);
+      }
+      if (value === BigInt(0)) {
+        throw new RuntimeError('division by 0');
+      }
+      state.environment.registers.set(ACCUMULATOR, accValue / value);
+      return new Ok();
+    }
+    default:
+      return new RuntimeError('invalid argumetn in instruction load');
+  }
+};
+
+ast.Read.prototype.interp = function (state) {
+  const arg: ast.Address | ast.Reference = this.argument;
+  switch (arg.constructor) {
+    case ast.Address: {
+      const value = state.environment.input.read();
+      state.environment.registers.set(arg.value, value);
+      return new Ok();
+    }
+    case ast.Reference: {
+      const readValue = state.environment.input.read();
+      let value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      value = state.environment.registers.get(value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + value);
+      }
+      state.environment.registers.set(value, readValue);
+      return new Ok();
+    }
+    default:
+      return new RuntimeError('invalid argumetn in instruction load');
+  }
+};
+
+ast.Write.prototype.interp = function (state) {
+  const arg: ast.Operandum = this.argument;
+  switch (arg.constructor) {
+    case ast.Const:
+      state.environment.output.write(arg.value);
+      return new Ok();
+    case ast.Address: {
+      const value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      state.environment.output.write(value);
+      return new Ok();
+    }
+    case ast.Reference: {
+      let value = state.environment.registers.get(arg.value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + arg.value);
+      }
+      value = state.environment.registers.get(value);
+      if (value == null) {
+        throw new RegisterError('empty register nr ' + value);
+      }
+      state.environment.output.write(value);
+      return new Ok();
+    }
+    default:
+      return new RuntimeError('invalid argumetn in instruction load');
+  }
+};
+
+ast.Jump.prototype.interp = function (state) {
+  const arg: ast.Label = this.argument;
+  const jumpTarget = state.program.labels.get(arg.value);
+  if (jumpTarget == null) {
+    throw new LabelError('unrecognized label ' + arg.value);
+  } else {
+    state.nextInstruction = jumpTarget;
+    return new Ok();
+  }
+};
+
+ast.Jgtz.prototype.interp = function (state) {
+  const arg: ast.Label = this.argument;
+  const jumpTarget = state.program.labels.get(arg.value);
+  if (jumpTarget == null) {
+    throw new LabelError('unrecognized label ' + arg.value);
+  } else {
+    const value = state.environment.registers.get(ACCUMULATOR);
+    if (value == null) {
+      throw new RegisterError('empty register nr ' + arg.value);
+    } else if (value > BigInt(0)) {
+      state.nextInstruction = jumpTarget;
+    }
+    return new Ok();
+  }
+};
+
+ast.Jzero.prototype.interp = function (state) {
+  const arg: ast.Label = this.argument;
+  const jumpTarget = state.program.labels.get(arg.value);
+  if (jumpTarget == null) {
+    throw new LabelError('unrecognized label ' + arg.value);
+  } else {
+    const value = state.environment.registers.get(ACCUMULATOR);
+    if (value == null) {
+      throw new RegisterError('empty register nr ' + arg.value);
+    } else if (value === BigInt(0)) {
+      state.nextInstruction = jumpTarget;
+    }
+    return new Ok();
+  }
+};
+
+ast.Halt.prototype.interp = function (state) {
+  state.completed = true;
+  return new Ok();
+};
+
+ast.Skip.prototype.interp = function (state) {
+  return new Ok();
+};
+
+ast.Combine.prototype.interp = function (state) {
+  state.nextInstruction = this.nextInstruction;
+  return this.instruction.interp(state);
+};
 
 export class Interpreter {
-  interpInstruction(instruction: ast.Instruction, state: State): Ok {
-    let actionResult: RuntimeError | Ok = new RuntimeError(
-      'unrecognized instruction'
-    );
-    if (instruction instanceof ast.Combine) {
-      state.nextInstruction = instruction.nextInstruction;
-      actionResult = this.interpInstruction(instruction.instruction, state);
-    } else if (instruction instanceof ast.Load) {
-      actionResult = inst.instructionLoad(instruction, state);
-    } else if (instruction instanceof ast.Store) {
-      actionResult = inst.instructionStore(instruction, state);
-    } else if (instruction instanceof ast.Add) {
-      actionResult = inst.instructionAdd(instruction, state);
-    } else if (instruction instanceof ast.Sub) {
-      actionResult = inst.instructionSub(instruction, state);
-    } else if (instruction instanceof ast.Mult) {
-      actionResult = inst.instructionMult(instruction, state);
-    } else if (instruction instanceof ast.Div) {
-      actionResult = inst.instructionDiv(instruction, state);
-    } else if (instruction instanceof ast.Read) {
-      actionResult = inst.instructionRead(instruction, state);
-    } else if (instruction instanceof ast.Write) {
-      actionResult = inst.instructionWrite(instruction, state);
-    } else if (instruction instanceof ast.Jump) {
-      actionResult = inst.instructionJump(instruction, state);
-    } else if (instruction instanceof ast.Jgtz) {
-      actionResult = inst.instructionJgtz(instruction, state);
-    } else if (instruction instanceof ast.Jzero) {
-      actionResult = inst.instructionJzero(instruction, state);
-    } else if (instruction instanceof ast.Halt) {
-      actionResult = inst.instructionHalt(state);
-    } else if (instruction instanceof ast.Skip) {
-      actionResult = inst.instructionSkip();
-    }
-
-    if (actionResult instanceof Ok) {
-      return actionResult;
-    } else {
-      throw actionResult;
-    }
-  }
-}
-
-export class InterpreterFast {
   interpInstruction(instruction: ast.Instruction, state: State): Ok {
     return instruction.interp(state);
   }
