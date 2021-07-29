8563a69604194605597ffdea4301f7dc7b222572
Andrzej Tkaczyk <309181@uwr.edu.pl>
2021-07-29 16:47:32 +0200

    rewrited App to class component lifted state up to App

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,43 +1,85 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Col, Container, Row} from 'react-bootstrap';
 import {Ddd} from './components/example-ddd';
+import {Engine, Interpreter, Ok, Parser, State} from './components/engine';
+import {OutputTape} from './components/outputTape';
 
-function App() {
-  return (
-    <div className="App">
-      <Container fluid>
-        <Row style={{height: '100vh'}}>
-          <Col sm={3}>
-            <Row style={{height: '10%'}}>
-              <Col style={{backgroundColor: 'lightblue'}}>aaa</Col>
-            </Row>
-            <Row style={{height: '90%'}}>
-              <Col style={{backgroundColor: 'green'}}>
-                <div>bbb</div>
-              </Col>
-            </Row>
-          </Col>
-          <Col sm={9}>
-            <Row style={{height: '10%'}}>
-              <Col>ccc</Col>
-            </Row>
-            <Row style={{height: '80%'}}>
-              <Col style={{backgroundColor: 'orange'}}>
-                <Ddd />
-              </Col>
-            </Row>
-            <Row style={{height: '10%'}}>
-              <Col style={{backgroundColor: 'yellow'}}>
-                <div>eee</div>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+const engine = new Engine(new Parser(), new Interpreter());
+const program = `
+ write =5
+ write =7
+`;
+
+class App extends Component<{}, {state: State}> {
+  constructor(props: {}) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.onClickRestart = this.onClickRestart.bind(this);
+
+    this.state = {state: engine.makeStateFromString(program, [])};
+  }
+
+  onClick = () => {
+    try {
+      const instructionResult: Ok = engine.stepInstruction(this.state.state);
+      this.setState(() => ({
+        state: instructionResult.state,
+      }));
+    } catch (err) {
+      // manage runtime errors
+    }
+  };
+
+  onClickRestart = () => {
+    this.setState(() => ({
+      state: engine.makeStateFromString(program, []),
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Container fluid>
+          <Row style={{height: '100vh'}}>
+            <Col sm={3}>
+              <Row style={{height: '10%'}}>
+                <Col style={{backgroundColor: 'lightblue'}}>aaa</Col>
+              </Row>
+              <Row style={{height: '90%'}}>
+                <Col style={{backgroundColor: 'green'}}>
+                  <div>bbb</div>
+                </Col>
+              </Row>
+            </Col>
+            <Col sm={9}>
+              <Row style={{height: '10%'}}>
+                <Col>ccc</Col>
+              </Row>
+              <Row style={{height: '80%'}}>
+                <Col style={{backgroundColor: 'orange'}}>
+                  <Ddd
+                    onClick={this.onClick}
+                    onClickRestart={this.onClickRestart}
+                    state={this.state.state}
+                    program={program}
+                  />
+                </Col>
+              </Row>
+              <Row style={{height: '10%'}}>
+                <Col style={{backgroundColor: 'yellow'}}>
+                  <OutputTape
+                    outs={this.state.state.environment.output.values}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
--- application/src/components/example-ddd.tsx
+++ application/src/components/example-ddd.tsx
@@ -1,47 +1,22 @@
 import React, {Component} from 'react';
-import {Engine, Interpreter, Ok, Parser, State} from './engine';
+import {State} from './engine';
 
-const engine = new Engine(new Parser(), new Interpreter());
-const program = `
- write =5
- write =7
-`;
-
-type RamState = {
+type dddProps = {
+  onClick: () => void;
+  onClickRestart: () => void;
   state: State;
+  program: string;
 };
-
-export class Ddd extends Component<{}, RamState> {
-  state: RamState = {
-    state: engine.makeStateFromString(program, []),
-  };
-
-  onClick = () => {
-    try {
-      const instructionResult: Ok = engine.stepInstruction(this.state.state);
-      this.setState(() => ({
-        state: instructionResult.state,
-      }));
-    } catch (err) {
-      // manage runtime errors
-    }
-  };
-
-  onClickRestart = () => {
-    this.setState(() => ({
-      state: engine.makeStateFromString(program, []),
-    }));
-  };
-
+export class Ddd extends Component<dddProps, {}> {
   render() {
     let button;
-    if (!this.state.state.completed) {
-      button = <button onClick={this.onClick}>Next</button>;
+    if (!this.props.state.completed) {
+      button = <button onClick={this.props.onClick}>Next</button>;
     } else {
       button = (
         <div>
           <button disabled>Koniec</button>
-          <button onClick={this.onClickRestart}>Restart</button>
+          <button onClick={this.props.onClickRestart}>Restart</button>
         </div>
       );
     }
@@ -49,9 +24,9 @@ export class Ddd extends Component<{}, RamState> {
     return (
       <div className="ddd-class">
         <p>
-          Program: <pre>{program}</pre>
+          Program: <pre>{this.props.program}</pre>
         </p>
-        <p>Output: [{this.state.state.environment.output.values.join(' ')}]</p>
+        <p>Output: [{this.props.state.environment.output.values.join(' ')}]</p>
         {button}
       </div>
     );
