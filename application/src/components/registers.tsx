5256bc218446837f5373de1b5b933844d1b1bedc
barnij <barnij@windowslive.com>
2022-01-04 00:00:53 +0100

    developed registers and changed es target in tsconfig

--- application/src/components/registers.tsx
+++ application/src/components/registers.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import {Table} from 'react-bootstrap';
 
-const Row = (props: {reg: bigint; value: bigint}) => (
+const Row = (props: {reg: bigint; value: bigint | undefined}) => {
+  let value = '?';
+  if (typeof props.value !== 'undefined') value = props.value.toString();
+
+  return (
+    <tr key={props.reg.toString()}>
+      <td>{props.reg.toString()}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const EmptyRow = () => (
   <tr>
-    <td>{props.reg.toString()}</td>
-    <td>{props.value.toString()}</td>
+    <td style={{textAlign: 'left'}} colSpan={2}>
+      ...
+    </td>
   </tr>
 );
 
@@ -14,12 +27,23 @@ interface IRegisterProps {
 
 export function Registers(props: IRegisterProps) {
   const rows: JSX.Element[] = [];
-  const sortedRegisters = [...props.registers.entries()].sort((a, b) =>
-    Number((a[0] - b[0]).toString())
+  const sortedRegisters = [...props.registers.keys()].sort((a, b) =>
+    Number(a - b)
   );
-  for (let i = 0; i < sortedRegisters.length; i++) {
-    const [reg, val] = sortedRegisters[i];
-    rows.push(<Row reg={reg} value={val} />);
+  const registers = new Set<bigint>([...Array(5).keys()].map(BigInt));
+  for (const reg of sortedRegisters) {
+    if (reg > 0n) registers.add(reg - 1n);
+    registers.add(reg);
+    registers.add(reg + 1n);
+  }
+
+  let prev = -1n;
+  for (const reg of registers) {
+    if (reg - prev > 2n) rows.push(<EmptyRow key={rows.length} />);
+    rows.push(
+      <Row key={rows.length} reg={reg} value={props.registers.get(reg)} />
+    );
+    prev = reg;
   }
 
   return (
