303c45faee134b6ae526e39914d28a426320a3b1
barnij <barnij@windowslive.com>
2022-02-01 00:21:25 +0100

    files reorganised - part 1

--- engine/src/index.ts
+++
@@ -1,7 +0,0 @@
-export {Engine} from './engine/engine';
-export {Interpreter} from './engine/interpreter';
-export {Parser} from './engine/parser';
-export {Ok, Break} from './engine/status';
-export {State} from './engine/environment';
-export * from './engine/errors';
-export * from './engine/ast';
